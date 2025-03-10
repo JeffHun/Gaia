851f1b6b4b0378a2e8d6f3f3080abafaafd76210
Pierre-Louis <plouis.angot@gmail.com>
2025-03-10 16:40:07 +0100

    [Feat] Started Traffic Manager implementation

--- Assets/Gaia/Scripts/Traffic System/Traffic Manager.cs
+++ Assets/Gaia/Scripts/Traffic System/Traffic Manager.cs
@@ -7,8 +7,26 @@ namespace Traffic
 
     public class TrafficManager : MonoBehaviour
     {
-        [SerializeField] private List<StopWaypoint> _stops;
-        [SerializeField] private List<CrosswalkWaypoint> _crosswalkWaypoints;
+        [SerializeField] private List<Waypoint> _roadInputWaypoints = new List<Waypoint>();
+        [SerializeField] private List<Waypoint> _roadOutputWaypoints = new List<Waypoint>();
+        [SerializeField] private List<Waypoint> _pedestrianInputWaypoints = new List<Waypoint>();
+        [SerializeField] private List<Waypoint> _pedestrianOutputWaypoints = new List<Waypoint>();
+        [SerializeField] private List<Waypoint> _bikeInputWaypoints = new List<Waypoint>();
+        [SerializeField] private List<Waypoint> _bikeOutputWaypoints = new List<Waypoint>();
+        [SerializeField] private List<Waypoint> _transportInputWaypoints = new List<Waypoint>();
+        [SerializeField] private List<Waypoint> _transportOutputWaypoints = new List<Waypoint>();
+
+        [SerializeField] private GameObject _pedestrianPool;
+        [SerializeField] private GameObject _roadPool;
+        [SerializeField] private GameObject _bikePool;
+        [SerializeField] private GameObject _transportPool;
+
+        [SerializeField, Range(0.0f, 1.0f)] private float _pedestrianRatio;
+        [SerializeField, Range(0.0f, 1.0f)] private float _roadRatio;
+        [SerializeField, Range(0.0f, 1.0f)] private float _bikeRatio;
+        [SerializeField, Range(0.0f, 1.0f)] private float _transportRatio;
+
+
     }
 
 }
