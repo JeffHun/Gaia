3f12f674a2973ec96d27dd5b8296b6cf0ae20a8d
Tyrm-Plouis <plouis.angot@gmail.com>
2025-04-09 16:45:09 +0200

    [Feat] Added XR Device Simulator for quick testing. Added Teleportation behaviour and anchors

---
+++ Assets/Gaia/Scripts/Teleportation/RayManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class RayManager : MonoBehaviour
+{
+    [SerializeField] private XRController _leftControllerRay;
+    [SerializeField] private XRController _rightControllerRay;
+    ActionBasedController _actionBasedController;
+    [SerializeField] private InputHelpers.Button _activationInput;
+    [SerializeField] private float _activationThreshold;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_leftControllerRay)
+        {
+            _leftControllerRay.gameObject.SetActive(CheckIfActivated(_leftControllerRay) && (!_rightControllerRay || !_rightControllerRay.gameObject.activeSelf));
+        }
+
+        if (_rightControllerRay)
+        {
+            _rightControllerRay.gameObject.SetActive(CheckIfActivated(_rightControllerRay) && (!_leftControllerRay || !_leftControllerRay.gameObject.activeSelf));
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, _activationInput, out bool isActivated, _activationThreshold);
+        return isActivated;
+    }
+
+}
