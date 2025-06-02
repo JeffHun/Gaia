800098031b556b5653666d673cd7429a3e55bd7c
Tyrm-Plouis <plouis.angot@gmail.com>
2025-06-02 16:40:57 +0200

    [Feat] Modified SUV model. Fixed Overview page update

--- Assets/Gaia/Scripts/Garage/UI/OverviewPage.cs
+++ Assets/Gaia/Scripts/Garage/UI/OverviewPage.cs
@@ -9,13 +9,13 @@ using Components;
 public class OverviewPage : MonoBehaviour
 {
     [SerializeField]
-    TextMeshProUGUI _typeNameText, _engineNameText, _optionsNameText, 
-        _typeFootprintText, _engineFootprintText, _optionsFootprintText, 
-        _typePriceText, _enginePriceText, _optionsPriceText, _footprintText, 
+    TextMeshProUGUI _typeNameText, _engineNameText, _settingsNameText, 
+        _typeFootprintText, _engineFootprintText, _settingsFootprintText, 
+        _typePriceText, _enginePriceText, _settingsPriceText, _footprintText, 
         _footprintBudgetText, _priceText, _priceBudgetText;
 
     [SerializeField]
-    Image _typeImage, _engineImage, _optionsImage;
+    Image _typeImage, _engineImage, _settingsImage;
     [SerializeField]
     Sprite _defaultImage;
 
@@ -24,6 +24,10 @@ public class OverviewPage : MonoBehaviour
 
     [SerializeField]
     UIManager _UIManager;
+
+    private int _typePrice = 0, _enginePrice = 0, _settingsPrice = 0;
+    private int _typeFootprint = 0, _engineFootprint = 0, _settingsFootprint = 0;
+
 
     private bool AreComponentsValid(ComponentData[] components)
     {
@@ -48,40 +52,37 @@ public class OverviewPage : MonoBehaviour
     {
         if (AreComponentsValid(components))
         {
-            /*_typeNameText.text = components[0].Name;
-            _engineNameText.text = components[1].Name;
-            _optionsNameText.text = components[2].Name;
+            _typeFootprint = components[0].GetComponentTotalFootprint();
+            _engineFootprint = components[1].GetComponentTotalFootprint();
+            _settingsFootprint = components[2].GetComponentTotalFootprint();
 
-            _typeImage.sprite = components[0].ImageSprite;
-            _engineImage.sprite = components[1].ImageSprite;
-            _optionsImage.sprite = components[2].ImageSprite;*/
-        
-            int typeTotal= components[0].GetComponentTotalFootprint();
-            int engineTotal= components[1].GetComponentTotalFootprint();
-            int optionsTotal = components[2].GetComponentTotalFootprint();
+            _typePrice = components[0].Price;
+            _enginePrice = components[1].Price;
+            _settingsPrice = components[2].Price;
 
-            _typeFootprintText.text = typeTotal.ToString();
-            _engineFootprintText.text = engineTotal.ToString();
-            _optionsFootprintText.text = optionsTotal.ToString();
-
-            _footprintBar.SetValues(typeTotal, engineTotal, optionsTotal);
-
-            _footprintText.text = (typeTotal + engineTotal + optionsTotal).ToString();
-            _footprintBudgetText.text = _UIManager.GetFootprintBudget().ToString();
-
-            typeTotal = components[0].Price;
-            engineTotal = components[1].Price;
-            optionsTotal = components[2].Price;
-
-            _typePriceText.text = typeTotal.ToString();
-            _enginePriceText.text = engineTotal.ToString();
-            _optionsPriceText.text = optionsTotal.ToString();
-
-            _priceBar.SetValues(typeTotal, engineTotal, optionsTotal);
-
-            _priceText.text = (typeTotal + engineTotal + optionsTotal).ToString();
-            _priceBudgetText.text = _UIManager.GetPriceBudget().ToString();
+            UpdateUI();
         }
+    }
+
+    public void UpdateUI()
+    {
+        _typeFootprintText.text = _typeFootprint.ToString();
+        _engineFootprintText.text = _engineFootprint.ToString();
+        _settingsFootprintText.text = _settingsFootprint.ToString();
+
+        _footprintBar.SetValues(_typeFootprint, _engineFootprint, _settingsFootprint);
+
+        _footprintText.text = (_typeFootprint + _engineFootprint + _settingsFootprint).ToString();
+        _footprintBudgetText.text = _UIManager.GetFootprintBudget().ToString();
+
+        _typePriceText.text = _typePrice.ToString();
+        _enginePriceText.text = _enginePrice.ToString();
+        _settingsPriceText.text = _settingsPrice.ToString();
+
+        _priceBar.SetValues(_typePrice, _enginePrice, _settingsPrice);
+
+        _priceText.text = (_typePrice + _enginePrice + _settingsPrice).ToString();
+        _priceBudgetText.text = _UIManager.GetPriceBudget().ToString();
     }
 
     public void UIAddComponent(ComponentData component)
@@ -91,15 +92,22 @@ public class OverviewPage : MonoBehaviour
             case Category.Type:
                 _typeNameText.text = component.Name;
                 _typeImage.sprite = component.ImageSprite;
+                _typeFootprint = component.GetComponentTotalFootprint();
+                _typePrice = component.Price;
                 break;
             case Category.Moteur:
                 _engineNameText.text = component.Name;
                 _engineImage.sprite = component.ImageSprite;
+                _engineFootprint = component.GetComponentTotalFootprint();
+                _enginePrice = component.Price;
                 break;
             case Category.Options:
-                _optionsNameText.text = component.Name;
-                _optionsImage.sprite = component.ImageSprite;
+                _settingsNameText.text = component.Name;
+                _settingsImage.sprite = component.ImageSprite;
+                _settingsFootprint = component.GetComponentTotalFootprint();
+                _settingsPrice = component.Price;
                 break;
         }
+        UpdateUI();
     }
 }
